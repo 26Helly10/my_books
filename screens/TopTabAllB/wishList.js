22d5dd5cf2b735df966494bcc7d01f5116abcf01
Elena <53377385+26Helly10@users.noreply.github.com>
2023-06-26 16:53:59 +0300

    ceva

--- screens/TopTabAllB/wishList.js
+++ screens/TopTabAllB/wishList.js
@@ -1,12 +1,9 @@
-import { View,Text} from "react-native";
+import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Wishlist (){
-    return(
-        <SafeAreaView>
-            <Text>
-                All Wishlist rgfbdfgb here
-            </Text>
-        </SafeAreaView>
+export default function Wishlist() {
+    return ( <SafeAreaView >
+        <Text >
+        All Wishlist here </Text> </SafeAreaView>
     )
 }
